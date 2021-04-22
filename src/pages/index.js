405f5dc1ebffe9812a3be9900378807c244ca2ae
Christophe Hadjigeorgiou <christophe.hadji@gmail.com>
2021-04-22 15:29:40 +0300

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,11 @@ import "./index.css";
 const IndexPage = () => {
   return (
     <div>
+      <script
+        data-ad-client="ca-pub-1208243264061405"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      ></script>
       <Page />
     </div>
   );
