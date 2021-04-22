f641030ccea36a312736f5eb5f1a927916be7903
Christophe Hadjigeorgiou <christophe.hadji@gmail.com>
2021-04-22 16:41:27 +0300

    update css lindos page

--- src/component/visite-page/lindos/Content.component.jsx
+++ src/component/visite-page/lindos/Content.component.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { DataContent } from "../../content/visite-page/Lindos.data";
 import "./Lindos.style.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import RuinImg from "../../../images/Lindos/Acropolis_Lindos_Rhodes_1.jpg";
 import Castle from "../../../images/Lindos/96696276.jpg";
 import FrontBeach from "../../../images/Lindos/Spiaggia-di-Lindos-Isola-di-Rodi.jpg";
-import BackBeach from "../../../images/Lindos/St.-Johns-Bay-Lindos-View.jpg";
 
 const Content = () => {
+
+  useEffect
+  (() => {
+    AOS.init({
+      duration: 1500,
+    });
+  },
+  []);
+
   return (
     <div>
       {DataContent.map((item) => {
         return (
           <>
-            <div className="title-container">
+            <div data-aos="fade-up" className="title-container">
               <Container>
                 <Row>
                   <Col>
-                    <h1>{item.guideTitle}</h1>
-                    <h5>{item.textTitle}</h5>
+                    <h1 className="title-guide">{item.guideTitle}</h1>
+                    <h5 className="sub-title-guide">{item.textTitle}</h5>
                   </Col>
                 </Row>
                 <Row>
@@ -29,25 +39,25 @@ const Content = () => {
                     <div>{item.histoireLindos}</div>
                   </Col>
                   <Col>
-                    <img src={RuinImg} alt="lindos" />
+                    <img className="image-ruin" src={RuinImg} alt="lindos" />
                   </Col>
                 </Row>
               </Container>
             </div>
-            <Container className="info-container" fluid>
+            <Container data-aos="fade-up" className="info-container" fluid>
               <Row className="info-black-background">
                 <Col className="info-text">{item.infoContainer}</Col>
               </Row>
             </Container>
             <Container>
-              <Row>
+              <Row data-aos="fade-up" className="text-container">
                 <Col>{item.textContent}</Col>
                 <Col>{item.textContentTwo}</Col>
               </Row>
             </Container>
             <div>
               <Container>
-                <Row>
+                <Row data-aos="fade-up">
                   <Col>
                     <img className="image" src={Castle} alt="lindos" />
                   </Col>
@@ -55,13 +65,18 @@ const Content = () => {
                     <img className="image" src={FrontBeach} alt="lindos" />
                   </Col>
                 </Row>
-                <Row>
+                <Row data-aos="fade-up" className="section-visite">
                   <Col>
-                    <script
-                      data-ad-client="ca-pub-1208243264061405"
-                      async
-                      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-                    ></script>
+                    <h3 className="section-visite-title">{item.lastOneTitle}</h3>
+                    <p>{item.lastOneText}</p>
+                  </Col>
+                  <Col>
+                    <h3 className="section-visite-title">{item.lastTwoTitle}</h3>
+                    <p>{item.lastTwoText}</p>
+                  </Col>
+                  <Col>
+                    <h3 className="section-visite-title">{item.lastThreeTitle}</h3>
+                    <p>{item.lastThreeText}</p>
                   </Col>
                 </Row>
               </Container>
